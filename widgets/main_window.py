eef37c6063419d66f71ee8ec8c01e3650e477bef
namirah rasul <n.rsl.136@gmail.com>
2024-04-21 01:09:57 +0600

    renamed ui things properly

--- widgets/main_window.py
+++ widgets/main_window.py
@@ -17,12 +17,8 @@ class UI_MainWindow(QMainWindow):
         uic.loadUi(Filepaths.MAIN_WINDOW(), self)
         self.setWindowTitle('Tune-it')
         # self.setFixedSize(1200, 700)
-
-        "self.plot_graph = pg.PlotWidget()"
-        "self.setCentralWidget(self.plot_graph)"
-        "time = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
-        "temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 30]"
-        "self.plot_graph.plot(time, temperature)"
+        
+        
 
         "initializing necessary objects"
         self.audio_file_path = None
